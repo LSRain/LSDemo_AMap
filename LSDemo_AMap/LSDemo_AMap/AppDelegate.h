3c308f22422cb7a7dd7eeac0235c75e5b2108a96
LSRain <315192042@qq.com>
2017-08-26 18:35:40 +0800

    project init

---
+++ LSDemo_AMap/LSDemo_AMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LSDemo_AMap
+//
+//  Created by WangBiao on 2017/8/26.
+//  Copyright © 2017年 LSRain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LSDemo_AMap/LSDemo_AMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LSDemo_AMap
+//
+//  Created by WangBiao on 2017/8/26.
+//  Copyright © 2017年 LSRain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
