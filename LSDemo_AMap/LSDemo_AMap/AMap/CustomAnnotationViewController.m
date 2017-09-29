3acc2912694b733c6075e092c79814fe6a9f7ccf
LSRain <315192042@qq.com>
2017-09-29 17:33:56 +0800

    modify some code

--- LSDemo_AMap/LSDemo_AMap/AMap/CustomAnnotationViewController.m
+++ LSDemo_AMap/LSDemo_AMap/AMap/CustomAnnotationViewController.m
@@ -7,16 +7,56 @@
 //
 
 #import "CustomAnnotationViewController.h"
+#import <MAMapKit/MAMapKit.h>
 
-@interface CustomAnnotationViewController ()
+@interface CustomAnnotationViewController ()<MAMapViewDelegate>
+
+@property (nonatomic, strong) MAMapView *mapView;
+@property (nonatomic, strong) NSMutableArray *annotations;
+@property (nonatomic, strong) MAAnnotationView *userLocationAnnotationView;
 
 @end
 
 @implementation CustomAnnotationViewController
 
+- (MAMapView *)mapView {
+    if (!_mapView) {
+        _mapView                      = [[MAMapView alloc] initWithFrame:self.view.bounds];
+        self.mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        _mapView.zoomLevel            = 18.0;
+        _mapView.desiredAccuracy      = kCLLocationAccuracyBestForNavigation;
+        _mapView.showsUserLocation    = YES;
+        _mapView.userTrackingMode     = MAUserTrackingModeFollow;
+        // close rotateCamera
+        _mapView.rotateCameraEnabled  = NO;
+        _mapView.showsScale           = NO;
+        _mapView.rotateEnabled        = NO;
+        _mapView.showsIndoorMap       = YES;
+        _mapView.showsCompass         = NO;
+        _mapView.delegate = self;
+        [self.view addSubview:_mapView];
+    }
+    return _mapView;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Back"
+                                                                             style:UIBarButtonItemStylePlain
+                                                                            target:self
+                                                                            action:@selector(returnAction)];
+    
+    [self mapView];
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"固定"
+                                                                              style:UIBarButtonItemStylePlain
+                                                                             target:self
+                                                                             action:@selector(lockAction)];
+    
+    [self initAnnotations];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +64,108 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self.mapView addAnnotations:self.annotations];
 }
-*/
+
+- (void)initAnnotations {
+    self.annotations = [NSMutableArray array];
+    
+    CLLocationCoordinate2D coordinates[2] = {
+        {
+            self.mapView.userLocation.coordinate.latitude + 0.00002,
+            self.mapView.userLocation.coordinate.longitude + 0.00002
+        },
+        {
+            self.mapView.userLocation.coordinate.latitude + 0.000004,
+            self.mapView.userLocation.coordinate.longitude + 0.000014,
+            
+        }
+    };
+    
+    
+    for (int i = 0; i < 2; ++i)
+    {
+        MAPointAnnotation *a = [[MAPointAnnotation alloc] init];
+        a.coordinate = coordinates[i];
+        [self.annotations addObject:a];
+        if(i == 0) {
+            a.lockedToScreen = YES;
+            a.lockedScreenPoint = CGPointMake(100, 100);
+        }
+    }
+}
+
+#pragma mark - event handling
+- (void)returnAction {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)lockAction {
+    MAPointAnnotation *an = self.annotations.firstObject;
+    
+    [an setLockedScreenPoint:CGPointMake(200, 200)];
+    [an setLockedToScreen:YES];
+}
+
+#pragma mark - MAMapviewDelegate
+// add
+- (void)mapView:(MAMapView *)mapView didUpdateUserLocation:(MAUserLocation *)userLocation updatingLocation:(BOOL)updatingLocation {
+    if (!updatingLocation && self.userLocationAnnotationView != nil)
+    {
+        [UIView animateWithDuration:0.1 animations:^{
+            
+            double degree = userLocation.heading.trueHeading - self.mapView.rotationDegree;
+            self.userLocationAnnotationView.transform = CGAffineTransformMakeRotation(degree * M_PI / 180.f );
+            
+        }];
+    }
+}
+
+- (MAAnnotationView *)mapView:(MAMapView *)mapView viewForAnnotation:(id<MAAnnotation>)annotation
+{
+    /*
+         - 自定义userLocation对应的annotationView.
+         - 这里涉及到一个代码上下顺序的问题
+     */
+    if ([annotation isKindOfClass:[MAUserLocation class]])
+    {
+        static NSString *userLocationStyleReuseIndetifier = @"userLocationStyleReuseIndetifier";
+        MAAnnotationView *annotationView = [mapView dequeueReusableAnnotationViewWithIdentifier:userLocationStyleReuseIndetifier];
+        if (annotationView == nil)
+        {
+            annotationView = [[MAAnnotationView alloc] initWithAnnotation:annotation  reuseIdentifier:userLocationStyleReuseIndetifier];
+        }
+        
+        annotationView.image = [UIImage imageNamed:@"userPosition"];
+        
+        self.userLocationAnnotationView = annotationView;
+        
+        return annotationView;
+    } else if ([annotation isKindOfClass:[MAPointAnnotation class]])
+    {
+        static NSString *pointReuseIdentifier = @"pointReuseIdentifier";
+        MAPinAnnotationView *annotationView = (MAPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:pointReuseIdentifier];
+        if (annotationView == nil)
+        {
+            annotationView = [[MAPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:pointReuseIdentifier];
+            
+            annotationView.canShowCallout            = YES;
+            annotationView.animatesDrop              = YES;
+            annotationView.draggable                 = YES;
+        }
+        
+        annotationView.pinColor = (annotation == self.annotations.firstObject ? MAPinAnnotationColorRed : MAPinAnnotationColorGreen);
+        
+        return annotationView;
+    }
+    
+    
+    return nil;
+}
 
 @end
+
